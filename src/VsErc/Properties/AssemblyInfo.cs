be8b59b51780946ad0ab9abe6baeeb3f074805ff
prabirshrestha <mail@prabir.me>
2013-08-22 19:18:36 -0400

    updated version to v0.0.1

--- src/VsErc/Properties/AssemblyInfo.cs
+++ src/VsErc/Properties/AssemblyInfo.cs
@@ -29,8 +29,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 
 [assembly: InternalsVisibleTo("VsErc_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006993cac777350178d1126d09a5265ba5a0cf329b102515c7b780ceebf466ca56446e460a82a8633fa3c205ea6146859639ac1563664a0b07a8f2add463d79f44e997939e8353dfdcfe9e6fc6dd73295aa5f6142480613499a3fb9ef69c7e66377041624010c9a8cae82b7da96f0af5ad0dd5a3371cc751d29869877bf4871fa9")]
 [assembly: InternalsVisibleTo("VsErc_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001006993cac777350178d1126d09a5265ba5a0cf329b102515c7b780ceebf466ca56446e460a82a8633fa3c205ea6146859639ac1563664a0b07a8f2add463d79f44e997939e8353dfdcfe9e6fc6dd73295aa5f6142480613499a3fb9ef69c7e66377041624010c9a8cae82b7da96f0af5ad0dd5a3371cc751d29869877bf4871fa9")]
