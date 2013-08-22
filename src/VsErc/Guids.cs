e787900a0a925e33553f29380bc9fe6366995d35
prabirshrestha <mail@prabir.me>
2013-08-22 19:16:37 -0400

    empty VsErc package

---
+++ src/VsErc/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace PrabirShrestha.VsErc
+{
+    static class GuidList
+    {
+        public const string guidVsErcPkgString = "d4448381-11e8-40b6-974f-d110a8904d79";
+        public const string guidVsErcCmdSetString = "746a062c-69f7-41df-8cd8-22dddda9fe2c";
+
+        public static readonly Guid guidVsErcCmdSet = new Guid(guidVsErcCmdSetString);
+    };
+}
