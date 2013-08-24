2a194ff256e8c0847d9bd95ce78af289ceba5fc3
prabirshrestha <mail@prabir.me>
2013-08-24 00:56:32 -0400

    remove snakecase for erc api

--- src/VsErc/scripts/init.lua
+++ src/VsErc/scripts/init.lua
@@ -1,19 +1,19 @@
-﻿local function script_path()
+﻿local function scriptPath()
    return debug.getinfo(2, "S").source:sub(2)
 end
 
-local script_dir = string.gsub(script_path(), "^(.+\\)[^\\]+$", "%1") 
+local scriptDir = string.gsub(scriptPath(), "^(.+\\)[^\\]+$", "%1") 
 
-local old_package_path = package.path
+local oldPackagePath = package.path
 
-package.path = package.path..';' .. script_dir .. '?.lua'
+package.path = package.path..';' .. scriptDir .. '?.lua'
 
 -- import packages here
 luanet.load_assembly "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
 require 'messagebox'
 
 -- restore the original package path
-package.path = old_package_path;
+package.path = oldPackagePath;
 
 if erc.MYERC then
     local myerc = assert(loadfile(erc.MYERC))
--- src/VsErc/scripts/messagebox.lua
+++ src/VsErc/scripts/messagebox.lua
@@ -2,10 +2,10 @@
 local MessageBoxButtons = luanet.import_type "System.Windows.Forms.MessageBoxButtons"
 local MessageBoxIcon    = luanet.import_type "System.Windows.Forms.MessageBoxIcon"
 
-function erc.message_box(message)
+function erc.messagebox(message)
     MessageBox.Show(message)
 end
 
-function erc.error_message_box(message)
+function erc.errormessagebox(message)
     MessageBox.Show(message, 'Error', MessageBoxButtons.OK, MessageBoxIcon.Error)
 end
