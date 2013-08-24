b39d9a9dcb3a3e5b1f1dca32c219b1dc9abe1437
prabirshrestha <mail@prabir.me>
2013-08-23 23:12:29 -0400

    erc.errorMessageBox

--- src/VsErc/scripts/init.lua
+++ src/VsErc/scripts/init.lua
@@ -1,15 +1,21 @@
 ﻿luanet.load_assembly "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
 
-if not erc.messageBox then
+if not erc.messageBox or not erc.errorMessage then
 
-	local MessageBox = luanet.import_type "System.Windows.Forms.MessageBox"
+    local MessageBox        = luanet.import_type "System.Windows.Forms.MessageBox"
+    local MessageBoxButtons = luanet.import_type "System.Windows.Forms.MessageBoxButtons"
+    local MessageBoxIcon    = luanet.import_type "System.Windows.Forms.MessageBoxIcon"
 
-	function erc.messageBox(message)
-		MessageBox.Show(message)
-	end
+    function erc.messageBox(message)
+        MessageBox.Show(message)
+    end
+
+    function erc.errorMessageBox(message)
+        MessageBox.Show(message, 'Error', MessageBoxButtons.OK, MessageBoxIcon.Error)
+    end
 
 end
 
 if erc.MYERC then
-	dofile(erc.MYERC)
+    dofile(erc.MYERC)
 end
