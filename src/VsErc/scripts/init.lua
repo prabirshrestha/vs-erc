9844e8d6f268014115ef2b5cd72efc25d030db62
prabirshrestha <mail@prabir.me>
2013-08-24 00:13:05 -0400

    update package.path temporariy to allow require

--- src/VsErc/scripts/init.lua
+++ src/VsErc/scripts/init.lua
@@ -1,20 +1,19 @@
-﻿luanet.load_assembly "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
-
-if not erc.messageBox or not erc.errorMessage then
-
-    local MessageBox        = luanet.import_type "System.Windows.Forms.MessageBox"
-    local MessageBoxButtons = luanet.import_type "System.Windows.Forms.MessageBoxButtons"
-    local MessageBoxIcon    = luanet.import_type "System.Windows.Forms.MessageBoxIcon"
-
-    function erc.messageBox(message)
-        MessageBox.Show(message)
-    end
-
-    function erc.errorMessageBox(message)
-        MessageBox.Show(message, 'Error', MessageBoxButtons.OK, MessageBoxIcon.Error)
-    end
-
+﻿local function script_path()
+   return debug.getinfo(2, "S").source:sub(2)
 end
+
+local script_dir = string.gsub(script_path(), "^(.+\\)[^\\]+$", "%1") 
+
+local old_package_path = package.path
+
+package.path = package.path..';' .. script_dir .. '?.lua'
+
+-- import packages here
+luanet.load_assembly "System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+require 'messagebox'
+
+-- restore the original package path
+package.path = old_package_path;
 
 if erc.MYERC then
     dofile(erc.MYERC)
---
+++ src/VsErc/scripts/messagebox.lua
@@ -0,0 +1,11 @@
+﻿local MessageBox        = luanet.import_type "System.Windows.Forms.MessageBox"
+local MessageBoxButtons = luanet.import_type "System.Windows.Forms.MessageBoxButtons"
+local MessageBoxIcon    = luanet.import_type "System.Windows.Forms.MessageBoxIcon"
+
+function erc.message_box(message)
+    MessageBox.Show(message)
+end
+
+function erc.error_message_box(message)
+    MessageBox.Show(message, 'Error', MessageBoxButtons.OK, MessageBoxIcon.Error)
+end
