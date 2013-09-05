c859b2d6eb69a0b4ce50ec5b3c6d3d6db3334886
prabirshrestha <mail@prabir.me>
2013-09-05 01:06:58 -0400

    IWpfTextViewCreationListener

---
+++ src/VsErc/Vs/VsWpfTextViewCreationListener.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace PrabirShrestha.VsErc.Vs
+{
+    [Export(typeof (IWpfTextViewCreationListener))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Document)]
+    public class JavaScriptSaveListener : IWpfTextViewCreationListener
+    {
+        public void TextViewCreated(IWpfTextView textView)
+        {
+	    Debug.WriteLine("loaded");
+        }
+    }
+}
