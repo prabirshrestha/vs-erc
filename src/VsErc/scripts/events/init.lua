7d9953981100bb7b7001925332a059a88b64bb93
prabirshrestha <mail@prabir.me>
2013-09-05 02:20:11 -0400

    support callback

--- src/VsErc/scripts/events/init.lua
+++ src/VsErc/scripts/events/init.lua
@@ -3,29 +3,33 @@ erc._editor.vs.events = {};
 
 do
 	
-	local eventsapi = {};
 	local events = {};
 
-	function eventsapi.register (eventlistener)
-		table.insert(events, eventlistener)
+	function erc.on (name, callback)
+		if callback then
+			local event = {}
+			event[name] = callback
+			table.insert(events, event)
+		else
+			table.insert(events, name)
+		end
 	end
-
-	erc.events = eventsapi;
 
 	function erc._editor.vs.events.onload_VsErcWpfTextViewCreationListener_TextCreated(textView)
 		for i,v in ipairs(events) do
-			if(v.onload) then
-				v.onload()
+			if(v.load) then
+				v.load()
 			end
 		end
 	end
 
 	function erc._editor.vs.events.onclose_VsErcWpfTextViewCreationListener_TextClosed(textView)
 		for i,v in ipairs(events) do
-			if(v.onclose) then
-				v.onclose()
+			if(v.close) then
+				v.close()
 			end
 		end
 	end
 
 end
+
