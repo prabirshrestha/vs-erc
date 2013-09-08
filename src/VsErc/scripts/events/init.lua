5be0bb3e22dd3c288b8600647c85b961a9775043
prabirshrestha <mail@prabir.me>
2013-09-08 13:59:27 -0400

    moved to new file onclose and onload

--- src/VsErc/scripts/events/init.lua
+++ src/VsErc/scripts/events/init.lua
@@ -1,35 +1,17 @@
-﻿erc._events = {};
-erc._editor.vs.events = {};
+﻿erc._events = {}
+erc._editor.vs.events = {}
 
-do
+erc._editor.vs.events._list = {}
 	
-	local events = {};
-
-	function erc.on (name, callback)
-		if callback then
-			local event = {}
-			event[name] = callback
-			table.insert(events, event)
-		else
-			table.insert(events, name)
-		end
+function erc.on (name, callback)
+	if callback then
+		local event = {}
+		event[name] = callback
+		table.insert(erc._editor.vs.events._list, event)
+	else
+		table.insert(erc._editor.vs.events._list, name)
 	end
-
-	function erc._editor.vs.events.onload_VsErcWpfTextViewCreationListener_TextCreated(textView)
-		for i,v in ipairs(events) do
-			if(v.load) then
-				v.load()
-			end
-		end
-	end
-
-	function erc._editor.vs.events.onclose_VsErcWpfTextViewCreationListener_TextClosed(textView)
-		for i,v in ipairs(events) do
-			if(v.close) then
-				v.close()
-			end
-		end
-	end
-
 end
 
+require('events/onload')
+require('events/onclose')
---
+++ src/VsErc/scripts/events/onload.lua
@@ -0,0 +1,7 @@
+﻿function erc._editor.vs.events.onload_VsErcWpfTextViewCreationListener_TextCreated(textView)
+	for i,v in ipairs(erc._editor.vs.events._list) do
+		if(v.load) then
+			v.load()
+		end
+	end
+end
