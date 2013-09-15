28c4e0e67d57d87e880ce8cce6b5ea0ebd01492f
prabirshrestha <mail@prabir.me>
2013-09-15 01:55:31 -0400

    project events lua bindings

---
+++ src/VsErc/scripts/events/vs/projectevents.lua
@@ -0,0 +1,24 @@
+﻿
+erc.editor.vs._vshelper.Events.PostProjectOpen:Add(function(o, e)
+	erc.emit('_vspostprojectopen')
+end)
+
+erc.editor.vs._vshelper.Events.QueryProjectClose:Add(function(o, e)
+	erc.emit('_vsqueryprojectclose')
+end)
+
+erc.editor.vs._vshelper.Events.PreProjectClose:Add(function(o, e)
+	erc.emit('_vspreprojectclose')
+end)
+
+erc.editor.vs._vshelper.Events.QueryProjectUnload:Add(function(o, e)
+	erc.emit('_vsqueryprojectunload')
+end)
+
+erc.editor.vs._vshelper.Events.PreProjectUnload:Add(function(o, e)
+	erc.emit('_vspreprojectunload')
+end)
+
+erc.editor.vs._vshelper.Events.PostProjectLoad:Add(function(o, e)
+	erc.emit('_vspostprojectload')
+end)
