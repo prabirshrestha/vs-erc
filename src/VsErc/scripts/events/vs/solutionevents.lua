4fbff6e33945314bf5cd3af22c35dc7bcf70417e
prabirshrestha <mail@prabir.me>
2013-09-15 01:47:07 -0400

    _vssolution events lua bindings

---
+++ src/VsErc/scripts/events/vs/solutionevents.lua
@@ -0,0 +1,16 @@
+﻿
+erc.editor.vs._vshelper.Events.PostSolutionOpen:Add(function(o, e)
+	erc.emit('_vspostsolutionopen')
+end)
+
+erc.editor.vs._vshelper.Events.QuerySolutionClose:Add(function(o, e)
+	erc.emit('_vsquerysolutionclose')
+end)
+
+erc.editor.vs._vshelper.Events.PreSolutionClose:Add(function(o, e)
+	erc.emit('_vspresolutionclose')
+end)
+
+erc.editor.vs._vshelper.Events.PostSolutionClose:Add(function(o, e)
+	erc.emit('_vspostsolutionclose')
+end)
